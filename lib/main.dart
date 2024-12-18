62dd60b254c321e74c21e9cd532a7b41316a2f68
mohil <mokariamohil@gmail.com>
2024-12-18 12:13:59 +0530

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:leading_dio/view/home.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      home: Home(),
+    ),
+  );
+}
